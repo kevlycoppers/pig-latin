8eadf1e432c96251a704ceccfc4e2ca039c91780
Levi Hopkins and Noah Ramey <noah@>
2016-06-08 16:30:17 -0700

    add basic specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,32 @@
 // BACK END //
 
-
 var consonantArray = []
 var vowelArray = ["a", "e", "i", "o", "u"]
 
 
-var pigLatin = function(sentence) {
-  if (sentence[0] === "a" || sentence[0] === "e" || sentence[0] === "i" || sentence[0] === "o" || sentence[0] === "u") {
-    var addAy = sentence + "ay";
+var pigLatin = function(word) {
+  if (word[0] === "a" || word[0] === "e" || word[0] === "i" || word[0] === "o" || word[0] === "u") {
+    var addAy = word + "ay";
     return addAy;
   } else {
-    // var char = sentence.split();
-    return sentence.slice(1) + sentence.charAt(0) + "ay"
+    while (word.charAt(0) !== "a" && word.charAt(0) !== "e" && word.charAt(0) !== "i" && word.charAt(0) !== "o" && word.charAt(0) !== "u") {
+      if (word.charAt(0) === "q") {
+        return word.slice(2) + word.charAt(0) + word.charAt(1) + "ay"
+      } else {
+        var consonant = word.substr(0,1)
+        return word.slice(1,word.length).concat(consonant)
+      }
+    }
   }
-}
-
-
-
+};
 
 // USER INTERFACE LOGIC //
 $(function() {
   $("form#piglatin").submit(function(event) {
     event.preventDefault();
 
-    var sentence = $("#english").val();
+    var word = $("#english").val();
 
-    $("#results").text(pigLatin(sentence));
+    $("#results").text(pigLatin(word));
   });
 });
