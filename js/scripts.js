87d7eabfd5c900d321db1779b2bcba4ab8c4e70f
Levi Hopkins and Noah Ramey <noah@>
2016-06-08 14:21:14 -0700

    add new a new rule and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,9 @@ var pigLatin = function(sentence) {
   if (sentence[0] === "a" || sentence[0] === "e" || sentence[0] === "i" || sentence[0] === "o" || sentence[0] === "u") {
     var addAy = sentence + "ay";
     return addAy;
+  } else {
+    // var char = sentence.split();
+    return sentence.slice(1) + sentence.charAt(0) + "ay"
   }
 }
 
